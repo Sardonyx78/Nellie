e177f669a50b3d23833742f792450d528f5cea83
= <sardonyx@sardonyx.me>
2022-05-07 02:49:29 +0300

    fix(Client): wait for the commands etc. to load before logging in

--- src/index.ts
+++ src/index.ts
@@ -10,26 +10,27 @@ import { DiscordEvent } from "./Structures/DiscordEvent";
 import { SlashCommand } from "./Structures/SlashCommand";
 import { MessageModal } from "./Structures/MessageModal";
 
-// Auto Import all Interactions from /interactions
-fs.readdirSync("./dist/interactions").filter(x => x.endsWith(".js")).forEach(x => {
-     const interaction = require(`./interactions/${x}`).default as InteractionApp<any>
-     client.interactions.set(interaction.typeName, interaction)
-})
+Promise.all([
+     // Auto Import all Interactions from /interactions
+     fs.readdirSync("./dist/interactions").filter(x => x.endsWith(".js")).forEach(async x => {
+          const interaction = (await import(`./interactions/${x}`)).default as InteractionApp<any>
+          client.interactions.set(interaction.typeName, interaction)
+     }),
 
-// Auto Import all SlashCommands from /commands
-fs.readdirSync("./dist/commands").filter(x => x.endsWith(".js")).forEach(x => {
-     const cmd = require(`./commands/${x}`).default as SlashCommand<any>
-     client.commands.set(cmd.name, cmd)
-})
+     // Auto Import all SlashCommands from /commands
+     fs.readdirSync("./dist/commands").filter(x => x.endsWith(".js")).map(async x => {
+          const cmd = (await import(`./commands/${x}`)).default as SlashCommand<any>
+          client.commands.set(cmd.name, cmd)
+     }),
 
-// Auto Import all DiscordEvents from /events
-fs.readdirSync("./dist/events").filter(x => x.endsWith(".js")).forEach(x => {
-     const event = require(`./events/${x}`).default as DiscordEvent<any>
-     client.on(event.eventName, event.handle)
-})
+     // Auto Import all DiscordEvents from /events
+     fs.readdirSync("./dist/events").filter(x => x.endsWith(".js")).map(async x => {
+          const event = (await import(`./events/${x}`)).default as DiscordEvent<any>
+          client.on(event.eventName, event.handle)
+     }),
 
-// Auto Import all Modals from /modals
-fs.readdirSync("./dist/modals").filter(x => x.endsWith(".js")).forEach(x => {
-     const cmd = require(`./modals/${x}`).default as MessageModal
-     client.modals.set(cmd.custom_id, cmd)
-})
+     // Auto Import all Modals from /modals
+     fs.readdirSync("./dist/modals").filter(x => x.endsWith(".js")).map(async x => {
+          const cmd = (await import(`./modals/${x}`)).default as MessageModal
+          client.modals.set(cmd.custom_id, cmd)
+     })]).then(() => client.login(process.env.TOKEN))
